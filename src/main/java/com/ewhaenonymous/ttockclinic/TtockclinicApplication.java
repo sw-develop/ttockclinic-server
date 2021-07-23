57b78c02e8762319b3d259eaf7d0435963466600
sw-develop <psw00719@naver.com>
2021-07-23 15:27:22 +0900

    first commit

---
+++ src/main/java/com/ewhaenonymous/ttockclinic/TtockclinicApplication.java
@@ -0,0 +1,13 @@
+package com.ewhaenonymous.ttockclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtockclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtockclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ewhaenonymous/ttockclinic/TtockclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewhaenonymous.ttockclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtockclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
