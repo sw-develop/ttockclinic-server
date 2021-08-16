0357f3be3ab0fed48d0b9ff943bcded6cb0a7459
sw-develop <psw00719@naver.com>
2021-08-16 10:14:02 +0900

    [DELETE] unused import statement

--- src/main/java/com/ewhaenonymous/ttockclinic/service/PaperService.java
+++ src/main/java/com/ewhaenonymous/ttockclinic/service/PaperService.java
@@ -6,7 +6,6 @@ import com.ewhaenonymous.ttockclinic.exception.ResourceNotFoundException;
 import com.ewhaenonymous.ttockclinic.payload.ResponseMessage;
 import com.ewhaenonymous.ttockclinic.repository.PaperRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
