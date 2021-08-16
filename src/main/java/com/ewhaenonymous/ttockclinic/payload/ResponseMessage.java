f2f22b59bc0c0e1a1c288155d11fbf9ba6d20eba
sw-develop <psw00719@naver.com>
2021-08-16 10:00:37 +0900

    [ADD] 응답메시지 - 유효하지 않은 QR 추가

---
+++ src/main/java/com/ewhaenonymous/ttockclinic/payload/ResponseMessage.java
@@ -0,0 +1,8 @@
+package com.ewhaenonymous.ttockclinic.payload;
+
+public final class ResponseMessage {
+    private ResponseMessage(){
+    }
+
+    public static final String INVALID_QR = "유효하지 않은 QR 입니다.";
+}
