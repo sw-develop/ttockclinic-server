e3faee0feb86c4a9c8a2807ca49ef8d15e744fb0
KJI-DEC <kji_dec22@ewhain.net>
2021-08-16 18:27:38 +0900

    [추가] CreateClinicRequest

--- src/main/java/com/ewhaenonymous/ttockclinic/payload/request/CreateClinicRequest.java
+++ src/main/java/com/ewhaenonymous/ttockclinic/payload/request/CreateClinicRequest.java
@@ -1,4 +1,20 @@
 package com.ewhaenonymous.ttockclinic.payload.request;
 
+import com.ewhaenonymous.ttockclinic.domain.Clinic;
+import com.ewhaenonymous.ttockclinic.domain.Paper;
+
+import javax.validation.constraints.NotNull;
+
 public class CreateClinicRequest {
+    @NotNull
+    private String latitude;
+    @NotNull
+    private String longitude;
+
+    public Clinic toEntity(){
+        return Clinic.builder()
+                .latitude(this.latitude)
+                .longitude(this.longitude)
+                .build();
+    }
 }
