b70528cbddd993e4c8a738cbb59b11eed0b99cc5
sw-develop <psw00719@naver.com>
2021-08-16 10:21:01 +0900

    [ADD] findByPhoneAndName 메서드

---
+++ src/main/java/com/ewhaenonymous/ttockclinic/repository/PaperRepository.java
@@ -0,0 +1,11 @@
+package com.ewhaenonymous.ttockclinic.repository;
+
+import com.ewhaenonymous.ttockclinic.domain.Paper;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface PaperRepository extends CrudRepository<Paper, Long> {
+
+    Optional<Paper> findByPhoneAndName(String phone, String name);
+}
