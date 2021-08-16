a3e858beea78d181df68cc7eb465944a57aec3aa
KJI-DEC <kji_dec22@ewhain.net>
2021-08-16 18:35:41 +0900

    [추가] GetClinicRequest

--- src/main/java/com/ewhaenonymous/ttockclinic/payload/request/GetClinicRequest.java
+++ src/main/java/com/ewhaenonymous/ttockclinic/payload/request/GetClinicRequest.java
@@ -1,4 +1,10 @@
 package com.ewhaenonymous.ttockclinic.payload.request;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class GetClinicRequest {
+    private int waitings;
 }
