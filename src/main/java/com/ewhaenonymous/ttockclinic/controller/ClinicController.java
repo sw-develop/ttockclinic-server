a8e73c6f24a33a702c65e89da2df4b3563e515ad
KJI-DEC <kji_dec22@ewhain.net>
2021-08-18 17:18:16 +0900

    [ADD] returnwaitings

--- src/main/java/com/ewhaenonymous/ttockclinic/controller/ClinicController.java
+++ src/main/java/com/ewhaenonymous/ttockclinic/controller/ClinicController.java
@@ -1,8 +1,11 @@
 package com.ewhaenonymous.ttockclinic.controller;
 
+import com.ewhaenonymous.ttockclinic.payload.request.GetClinicRequest;
+import com.ewhaenonymous.ttockclinic.payload.response.ClinicResponse;
 import com.ewhaenonymous.ttockclinic.service.ClinicService;
 import com.ewhaenonymous.ttockclinic.domain.Clinic;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,15 +18,9 @@ import javax.validation.Valid;
 public class ClinicController {
     private ClinicService clinicService;
 
-    @PostMapping("/papers")
-    public ResponseEntity<?> create(@RequestBody @Valid Clinic clinic){
-        clinicService.createNewClinic(clinic);
-        return ResponseEntity<>()
-    }
-
     @GetMapping("/clinics/waitings")
-    int returnwatings(@RequestBody @Valid Clinic clinic){
-
-        return clinicService.returnwaiting(clinic);
+    public ResponseEntity<?> returnwaitings(@RequestBody @Valid GetClinicRequest clinicRequest){
+        ClinicResponse clinicResponse = clinicService.returnwaiting(clinicRequest.getLongitude(), clinicRequest.getLatitude());
+        return new ResponseEntity<>(clinicResponse, HttpStatus.OK);
     }
 }
