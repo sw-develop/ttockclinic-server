01a208b4d12b04fa003db71944294f1e1760c317
KJI-DEC <kji_dec22@ewhain.net>
2021-08-19 13:33:27 +0900

    [추가] saveclinicpaper

--- src/main/java/com/ewhaenonymous/ttockclinic/controller/ClinicController.java
+++ src/main/java/com/ewhaenonymous/ttockclinic/controller/ClinicController.java
@@ -1,9 +1,11 @@
 package com.ewhaenonymous.ttockclinic.controller;
 
+import com.ewhaenonymous.ttockclinic.payload.request.CreateClinicRequest;
 import com.ewhaenonymous.ttockclinic.payload.request.GetClinicRequest;
 import com.ewhaenonymous.ttockclinic.payload.response.ClinicResponse;
+import com.ewhaenonymous.ttockclinic.payload.response.PaperResponse;
 import com.ewhaenonymous.ttockclinic.service.ClinicService;
-import com.ewhaenonymous.ttockclinic.domain.Clinic;
+import com.ewhaenonymous.ttockclinic.service.PaperService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,14 @@ import javax.validation.Valid;
 
 public class ClinicController {
     private ClinicService clinicService;
+    private PaperService paperService;
+
+    @PostMapping("/papers")
+    public ResponseEntity<?> saveclinicpaper(@RequestBody @Valid CreateClinicRequest createRequest){
+        clinicService.createNewClinic(createRequest);
+        paperService.createNewPaper(createRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
     @GetMapping("/clinics/waitings")
     public ResponseEntity<?> returnwaitings(@RequestBody @Valid GetClinicRequest clinicRequest){
