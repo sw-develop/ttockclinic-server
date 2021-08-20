e9ae5d7f05b4c4cf5bc3bc9883e1e8a81a5fbd2a
sw-develop <psw00719@naver.com>
2021-08-20 15:55:03 +0900

    [삭제] 사용하지 않은 메서드 삭제

--- src/main/java/com/ewhaenonymous/ttockclinic/repository/PaperRepository.java
+++ src/main/java/com/ewhaenonymous/ttockclinic/repository/PaperRepository.java
@@ -3,10 +3,12 @@ package com.ewhaenonymous.ttockclinic.repository;
 import com.ewhaenonymous.ttockclinic.domain.Paper;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface PaperRepository extends CrudRepository<Paper, Long> {
 
-    Optional<Paper> findByPhoneAndName(String phone, String name);
-    Optional<Paper> findByPhone(String phone);
+    Optional<Paper> findByPhoneAndNameAndDeleted(String phone, String name, String deleted);
+    Optional<Paper> findByPhoneAndDate(String phone, LocalDate date);
+
 }
