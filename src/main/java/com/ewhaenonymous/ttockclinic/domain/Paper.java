9683f20e9371a9acee6ce907f2711fc8cfeac344
sw-develop <psw00719@naver.com>
2021-08-24 00:03:30 +0900

    [추가] 모든 필드에 대한 생성자

--- src/main/java/com/ewhaenonymous/ttockclinic/domain/Paper.java
+++ src/main/java/com/ewhaenonymous/ttockclinic/domain/Paper.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 @Getter @Setter
 @Builder(access = AccessLevel.PUBLIC)
 @NoArgsConstructor
+@AllArgsConstructor
 public class Paper extends BaseTimeEntity {
 
     @Id
@@ -24,6 +25,7 @@ public class Paper extends BaseTimeEntity {
     @Column(name = "qr_usage_count")
     private int qrUsageCount;
 
+    //이게 객체 생성 날짜랑 똑같은 거 아닌가?
     @CreatedDate
     private LocalDate date;
 
