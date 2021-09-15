329cd91a62c8b84d533afb1f5ec3f19d1f3ad685
sw-develop <psw00719@naver.com>
2021-09-15 07:35:23 +0000

    [수정] domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(ttockclinic.com www.ttockclinic.com)
+domains=(www.ttockclinic.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="anikapsw@gmail.com" # Adding a valid address is strongly recommended
