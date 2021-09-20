698eaf87e01aa722c59c240a2440f6f76610d842
sw-develop <psw00719@naver.com>
2021-09-21 01:24:11 +0900

    [수정] multiple CrossOrigin urls

--- src/main/java/com/ewhaenonymous/ttockclinic/controller/PaperController.java
+++ src/main/java/com/ewhaenonymous/ttockclinic/controller/PaperController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@CrossOrigin(origins = "http://localhost:19006, http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:19006", "http://localhost:3000"})
 @RequestMapping("/v1")
 @RestController
 @RequiredArgsConstructor
