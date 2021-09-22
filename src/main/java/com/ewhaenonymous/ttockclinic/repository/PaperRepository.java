20bc08545f15717b90d3ebe97c407829e2a7a757
KJI-DEC <kji_dec22@ewhain.net>
2021-09-22 20:29:59 +0900

    [추가] Paper Repository

---
+++ src/main/java/com/ewhaenonymous/ttockclinic/repository/PaperRepository.java
@@ -0,0 +1,13 @@
+package com.ewhaenonymous.ttockclinic.repository;
+
+import com.ewhaenonymous.ttockclinic.domain.Paper;
+import org.springframework.data.repository.CrudRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface PaperRepository extends CrudRepository<Paper, Long> {
+    Optional<Paper> findByPhoneAndNameAndDeleted(String phone, String name, String deleted);
+    Optional<Paper> findByPhoneAndDate(String phone, LocalDate date);
+    Optional<Paper> findByIdAndDeleted(Long id, String deleted);
+}
