2b834ed1b890a4a318d2e66a1d9441ef95578796
sw-develop <psw00719@naver.com>
2021-09-22 21:57:55 +0900

    [추가] Clinic 리스트 반환용 response

---
+++ src/main/java/com/ewhaenonymous/ttockclinic/payload/ClinicListResponse.java
@@ -0,0 +1,22 @@
+package com.ewhaenonymous.ttockclinic.payload;
+
+import com.ewhaenonymous.ttockclinic.domain.Clinic;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClinicListResponse {
+    private Long id;
+    private String siDo;
+    private String siGunGu;
+    private String name;
+    private String address;
+    private int waitings;
+
+    public ClinicListResponse(Clinic clinic){
+        this(clinic.getId(), clinic.getSiDo(), clinic.getSiGunGu(), clinic.getName(), clinic.getAddress(), clinic.getWaitings());
+    }
+}
