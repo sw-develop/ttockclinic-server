513b3eb94555a65a5c1c24b3d3f34075ec70ee5b
KJI-DEC <kji_dec22@ewhain.net>
2021-09-22 20:28:50 +0900

    [추가] Paper 응답 payload

---
+++ src/main/java/com/ewhaenonymous/ttockclinic/payload/response/PaperResponse.java
@@ -0,0 +1,25 @@
+package com.ewhaenonymous.ttockclinic.payload.response;
+
+import com.ewhaenonymous.ttockclinic.domain.Clinic;
+import com.ewhaenonymous.ttockclinic.domain.Paper;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaperResponse {
+    private long id;
+    private String name;
+    private String phone;
+    private LocalDate date;
+    private String deleted;
+    private Clinic clinic;
+
+    public PaperResponse(Paper paper){
+        this(paper.getId(), paper.getName(), paper.getPhone(), paper.getDate(),paper.getDeleted(), paper.getClinic());
+    }
+}
