27d3fef4248c4e199b62c37bff898283cdfe64dd
sw-develop <psw00719@naver.com>
2021-09-22 23:26:22 +0900

    [추가] native query 작성

--- src/main/java/com/ewhaenonymous/ttockclinic/ClinicRepository.java
+++ src/main/java/com/ewhaenonymous/ttockclinic/ClinicRepository.java
@@ -1,6 +1,7 @@
 package com.ewhaenonymous.ttockclinic;
 
 import com.ewhaenonymous.ttockclinic.domain.Clinic;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,5 +9,7 @@ import java.util.Optional;
 
 public interface ClinicRepository extends CrudRepository<Clinic, Long> {
     Optional<Clinic> findById(Long id);
+
+    @Query(value = "select * from clinic where si_do = ?1 and (si_gun_gu like %?2%);", nativeQuery = true)
     List<Clinic> findBySiDoAndSiGunGuContaining(String siDo, String siGunGu); //확인하기!
 }
