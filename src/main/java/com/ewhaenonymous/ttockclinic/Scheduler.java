6b5448763c35a8fea55b790aea64d9fc2a6f9393
KJI-DEC <kji_dec22@ewhain.net>
2021-09-25 21:55:09 +0900

    [추가] 매일 자정 db 삭제 스케줄러

---
+++ src/main/java/com/ewhaenonymous/ttockclinic/Scheduler.java
@@ -0,0 +1,22 @@
+package com.ewhaenonymous.ttockclinic;
+
+import com.ewhaenonymous.ttockclinic.domain.Paper;
+import com.ewhaenonymous.ttockclinic.repository.PaperRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import java.util.Stack;
+
+@Component
+@Slf4j
+public class Scheduler {
+    PaperRepository paperRepository;
+    @Scheduled(cron = "0 0 0 * * *")	// 매일 00시 정각마다 db 삭제
+    public void deleteData() throws Exception {
+        Stack<Paper> paperStack = paperRepository.findByDeleted("N");
+        while(!paperStack.isEmpty()){
+            Paper p = paperStack.pop();
+            p.setDeleted("Y");
+        }
+    }
+}
