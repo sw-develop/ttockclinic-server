8fe2f5dc4b506ece4aac13948ed84daa28960e84
KJI-DEC <kji_dec22@ewhain.net>
2021-09-26 02:01:33 +0900

    [추가] Service 어노테이션 추가

--- src/main/java/com/ewhaenonymous/ttockclinic/Scheduler.java
+++ src/main/java/com/ewhaenonymous/ttockclinic/Scheduler.java
@@ -6,11 +6,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.util.Stack;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
+@Service
 public class Scheduler {
 
     private final PaperRepository paperRepository;
