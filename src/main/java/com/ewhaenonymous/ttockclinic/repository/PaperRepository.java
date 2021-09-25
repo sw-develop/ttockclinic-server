626bb8ab30de0b51094b554a64112c1ec6baf794
KJI-DEC <kji_dec22@ewhain.net>
2021-09-25 21:55:35 +0900

    [수정] findByDeleted 추가

--- src/main/java/com/ewhaenonymous/ttockclinic/repository/PaperRepository.java
+++ src/main/java/com/ewhaenonymous/ttockclinic/repository/PaperRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
 import java.util.Optional;
+import java.util.Stack;
 
 public interface PaperRepository extends CrudRepository<Paper, Long> {
     Optional<Paper> findByPhoneAndNameAndDeleted(String phone, String name, String deleted);
     Optional<Paper> findByPhoneAndDate(String phone, LocalDate date);
     Optional<Paper> findByIdAndDeleted(Long id, String deleted);
+    Stack<Paper> findByDeleted(String deleted);
 }
