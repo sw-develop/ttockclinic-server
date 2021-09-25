bcc2f1f3e737b3f70683522edd9ea56923888dd8
KJI-DEC <kji_dec22@ewhain.net>
2021-09-25 21:55:50 +0900

    [수정] 스케줄러 어노테이션 추가

--- src/main/java/com/ewhaenonymous/ttockclinic/TtockclinicApplication.java
+++ src/main/java/com/ewhaenonymous/ttockclinic/TtockclinicApplication.java
@@ -3,9 +3,11 @@ package com.ewhaenonymous.ttockclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing //JPA Auditing 활성화를 위한 어노테이션
 @SpringBootApplication
+@EnableScheduling	//자정마다 데이터 삭제 처리
 public class TtockclinicApplication {
 
 	public static void main(String[] args) {
